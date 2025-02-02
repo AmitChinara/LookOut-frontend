a5a9a73abf96a5db063b9e8efc7a4989d6e39514
Amit Chinara <amitchinara@gmail.com>
2025-02-02 16:47:25 +0530

    Change 01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,11 +33,13 @@ const Home: React.FC = () => {
                         message._id === response.id ? { ...message, status: response.newStatus } : message
                     )
                 );
+                setLogData(prevlog => [...prevlog, response.data.logData]);
             } else if (response?.event === "SERVICE_CREATED") {
                 setMessages(prevMessages => [...prevMessages, response.data.statusData]);
-                setLogData(prevMessages => [...prevMessages, response.data.logData]);
+                setLogData(prevlog => [...prevlog, response.data.logData]);
             } else if (response?.event === "SERVICE_DELETED") {
                 setMessages(prevMessages => prevMessages.filter(message => message._id !== response.id));
+                setLogData(prevlog => [...prevlog, response.data.logData]);
             }
         };
 
